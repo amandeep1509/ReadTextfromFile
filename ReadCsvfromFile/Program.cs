ba3d68d48371ea7199e8db2125b4d51e428d5062
amandeep1509 <asing609@my.centennialcollege.ca>
2016-11-08 23:07:15 -0500

    read the input stream from a text file

---
+++ ReadCsvfromFile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCsvfromFile
+{
+    class Program
+    {
+        static string theLine;
+        static int[] theList;
+        static int LineNumber = 1;
+        static int theSum;
+        static StreamReader file = new StreamReader("C: /Users/amand/OneDrive/Documents/visual studio 2015/Projects/ReadCsvfromFile/TextFiletobeRead.txt");
+        static void Main(string[] args)
+        {
+            while ((theLine = file.ReadLine()) != null)
+            {
+                Add(theLine);
+            }
+            file.Close();
+
+            Console.ReadLine();
+        }
+
+        private static void Add(string theLine)
+        {
+            if (theLine.Contains(";"))
+            {
+                theList = theLine.Split(';').Select(int.Parse).ToArray();
+            }
+            else if (theLine.Contains(","))
+            {
+                theList = theLine.Split(',').Select(int.Parse).ToArray();
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            theSum = theList.Sum();
+
+            Console.WriteLine("The sum of the numbers entered at line : " + LineNumber.ToString() + " is : " + theSum);
+            LineNumber++;
+        }
+    }
+}
